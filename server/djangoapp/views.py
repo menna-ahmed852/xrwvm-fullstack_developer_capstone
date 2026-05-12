b50f11ffb3d232957a19cc7dd7487d71e2f04ef8
menna-ahmed852 <menna.ahmed@pwc.com>
2026-05-12 19:33:44 -0400

    fix python lint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -132,10 +132,12 @@ def add_review(request):
     if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-           post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse(
+                {"status": 401, "message": "Error in posting review"}
+            )
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
 
