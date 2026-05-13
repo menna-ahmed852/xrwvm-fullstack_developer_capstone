85353d30f987390e6ea799c9adef5ffdd85b5eb8
menna-ahmed852 <menna.ahmed@pwc.com>
2026-05-12 20:09:07 -0400

    lint fix

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -10,15 +10,15 @@ def initiate():
         {"name": "Toyota", "description": "Great cars. Japanese technology"},
     ]
 
-car_make_instances = []
+    car_make_instances = []
 
-for data in car_make_data:
-    car_make_instances.append(
-        CarMake.objects.create(
-            name=data["name"],
-            description=data["description"],
+    for data in car_make_data:
+        car_make_instances.append(
+            CarMake.objects.create(
+                name=data["name"],
+                description=data["description"],
+            )
         )
-    )
 
     car_model_data = [
         {
@@ -57,9 +57,13 @@ for data in car_make_data:
             "year": 2023,
             "car_make": car_make_instances[1],
         },
-        {"name": "A4", "type": "SUV", "year": 2023,
-         "car_make": car_make_instances[2]},
-       {
+        {
+            "name": "A4",
+            "type": "SUV",
+            "year": 2023,
+            "car_make": car_make_instances[2],
+        },
+        {
             "name": "A5",
             "type": "SUV",
             "year": 2023,
@@ -107,7 +111,6 @@ for data in car_make_data:
             "year": 2023,
             "car_make": car_make_instances[4],
         },
-        # Add more CarModel instances as needed
     ]
 
     for data in car_model_data:
